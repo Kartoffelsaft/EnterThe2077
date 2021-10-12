2ad3886ad2a4ab488ff63dbee9ddb4f844f596f2
Benjamin Findley <ben.g.findley@gmail.com>
2021-10-12 15:29:27 -0700

    zooming

--- src/player/camera.gd
+++ src/player/camera.gd
@@ -1,6 +1,9 @@
 extends Camera
 
 export var rotateSensitivity = 0.003;
+export var minZoom = 5.0;
+export var maxZoom = 30.0;
+export var zoomSensitivity = 0.1;
 
 export var screenShakeAmountPos = 0.2
 export var screenShakeAmountRot = 0.03
@@ -21,6 +24,18 @@ func _unhandled_input(event):
     if(event is InputEventMouseMotion
     && Input.is_action_pressed("camera_rotate")):
         restingPosition = restingPosition.rotated(Vector3(0, 1, 0), event.relative.x * rotateSensitivity)
+    
+    if(event.is_action("camera_in")):
+        restingPosition = Transform(restingPosition.basis, restingPosition.origin * (1 - zoomSensitivity))
+    if(event.is_action("camera_out")):
+        restingPosition = Transform(restingPosition.basis, restingPosition.origin * (1 + zoomSensitivity))
+    
+    if(restingPosition.origin.length_squared() < minZoom * minZoom):
+        restingPosition = Transform(restingPosition.basis, restingPosition.origin.normalized() * minZoom)
+    if(restingPosition.origin.length_squared() > maxZoom * maxZoom):
+        restingPosition = Transform(restingPosition.basis, restingPosition.origin.normalized() * maxZoom)
+
+
 
 func shake():
     var posOffset = Vector3(rand_range(-screenShakeAmountPos, screenShakeAmountPos), rand_range(-screenShakeAmountPos, screenShakeAmountPos), rand_range(-screenShakeAmountPos, screenShakeAmountPos))
